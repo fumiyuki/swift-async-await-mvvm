a247da12698d4a8891166974af4216928c0ee3d8
Fumiyuki Otsuka <fumiyuki@users.noreply.github.com>
2021-09-08 04:27:25 +0900

    Initial Commit

---
+++ swift-async-await-mvvm/swift_async_await_mvvmApp.swift
@@ -0,0 +1,18 @@
+//
+//  swift_async_await_mvvmApp.swift
+//  swift-async-await-mvvm
+//
+//  Created by Fumiyuki Otsuka on 2021/09/08.
+//  
+
+
+import SwiftUI
+
+@main
+struct swift_async_await_mvvmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
